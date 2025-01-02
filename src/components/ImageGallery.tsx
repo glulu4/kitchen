05488b0079df957a24212974c8f26d055dcd5e0e
Gabriel Lulu <glulu4444@gmail.com>
2025-01-02 15:22:21 -0500

    apos

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -5,53 +5,53 @@ import React from "react";
 const mediaUrls = [
   [
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     },
     {
-      type: "video",
+      type: "video" as const,
       url: "https://www.w3schools.com/html/mov_bbb.mp4", // Example video URL
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
     },
   ],
   [
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://docs.material-tailwind.com/img/team-3.jpg",
     },
   ],
   [
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://docs.material-tailwind.com/img/team-3.jpg",
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
   ],
   [
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
     {
-      type: "image",
+      type: "image" as const,
       url: "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
     },
   ],
